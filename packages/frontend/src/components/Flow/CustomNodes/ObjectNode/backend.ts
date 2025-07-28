592a8732c89f85e00ab9705cfe4371f9552d7082
Jan Johansen <jan@johansenweb.dk>
2025-07-28 22:46:19 +0200

    Removed old location for custom components.

--- packages/frontend/src/components/Flow/CustomNodes/ObjectNode/backend.ts
+++
@@ -1,8 +0,0 @@
-import { NodeBackendBaseV1 } from "../../backend_types"
-
-export default class ObjectNode extends NodeBackendBaseV1 {
-    setup() {
-        // Implementation for setup
-        console.log("Setting up Object node:", this.context)
-    }
-}
